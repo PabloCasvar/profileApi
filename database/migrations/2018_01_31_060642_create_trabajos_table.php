1a1431be6656de8ec5d305d2ccfd2161bedb179d
PabloCasvar <casvar.pablo@gmail.com>
2018-01-31 01:26:27 -0600

    Adicion de Modelos, Controladores, Migraciones y Seeders

---
+++ database/migrations/2018_01_31_060642_create_trabajos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrabajosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('empresa', 100);
+            $table->string('puesto', 100); 
+            $table->text('descripcion');
+            $table->string('periodo', 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajos');
+    }
+}
